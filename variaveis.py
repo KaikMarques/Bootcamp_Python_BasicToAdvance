063c78b56803c2c576a595a5ce9aa8d0878e43df
Kaik Xavier Marques de Oliveira <kaikmarques.ti@hotmail.com>
2024-03-14 22:56:59 -0300

    variaveis
    Estudos sobre variáveis e tipos de dados

---
+++ variaveis.py
@@ -0,0 +1,22 @@
+# Uma Variável é um local reservado na memória do computador que é usado para armazenar de forma temporária os dados que o programa utiliza
+
+# media = 0
+# n1 = n2 = n3 = n4 = 0.0
+# nome, idade = 'Fábio', 47
+
+# Função type()
+# print(type(media))
+# print(type(n2))
+# print(type(nome))
+# print(type(estado))
+# print(type(1+2j))
+
+# Função isistance() é uma ocorrência de uma determinada classe/tipo. ELa retorna verdadeiro ou falso dependendo do tipo da minha variavel que eu especificar
+# a = 10
+# b = 'Sol'
+# print(isinstance(a, (int, float))) #quer dizer: a variavel "a" é do tipo int ou float 
+
+a = 40
+c = 3
+r = a * c
+print(r)
